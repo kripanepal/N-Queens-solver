8ca05279883dd7820f2612d547004ad4c5f31928
Kripa Nepal <kripanepal20@gmail.com>
2020-03-17 15:06:27 -0500

    initial commit

---
+++ Test.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Test {
+
+   
+
+    public static void main(String[] args){
+
+	solveQueens temp = new solveQueens(16);
+	temp.showBoard();
+	
+
+	
+
+    }
+
+   
+
+}
---
+++ solveQueens.java
@@ -0,0 +1,108 @@
+import java.util.Arrays;
+
+public class solveQueens {
+    
+    public  boolean[][] board;
+    public  boolean solved = false;
+    
+    
+    public solveQueens(int num)
+    {
+	board = new boolean[num][num];
+	solve(num,0);
+	
+	
+    }
+    
+    public void solve(int n, int row){
+
+	
+   	if(row < n)
+   	    {
+
+   		for(int j = 0; j < n; j++)
+   		    {
+   			if(!solved)
+   			    {
+
+   				if(check(row, j))
+   				    {
+   					
+   					board[row][j] = true;
+
+   					if(row == 3)
+   					    {
+   						solved = true;
+   					    }else
+   					    {
+   						solve(n, row + 1);
+   					    }
+
+   				    }
+   				
+   				if(!solved)
+   				    {
+
+   					board[row][j] = false;
+   				    }
+
+   			    }
+
+   		    }
+
+   	    }
+
+       }
+
+       public  boolean check(int row, int col){
+
+   	for(int i = 0; i < row; i++)
+   	    {
+   		for(int j = 0; j < board[0].length; j++)
+   		    {
+   			
+
+   			if(board[i][j])
+   			    {
+
+   				if((i == row) || (j == col) || (i - j) == (row - col) || ((i + j) == (row + col)))
+   				    {
+   					return false;
+   				    }
+   			    }
+   		    }
+   	    }
+   	return true;
+
+       }
+
+    public void showBoard(){
+	
+	
+	
+	for(int i =0; i< board[0].length;i++)
+	{
+	    
+	    System.out.print("|");
+	    for(int j =0; j< board[0].length;j++)
+		{
+		    
+		    if(board[i][j])
+		    {
+			System.out.print("Q");
+		    }
+		    else
+			{
+			    System.out.print("-");
+			}
+		    System.out.print(" | ");
+		    
+		}
+	    System.out.println();
+	    
+	    
+	}
+	
+	
+    }
+}
